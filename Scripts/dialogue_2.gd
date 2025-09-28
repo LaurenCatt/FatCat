8ecf87ff38010a33c828d4d650c0f2725d24dff0
LaurenCatt <laurenknowles2772@outlook.com>
2025-09-28 00:26:08 -0400

    help

--- Scripts/dialogue_2.gd
+++ Scripts/dialogue_2.gd
@@ -1,1 +1,8 @@
 extends Control
+
+var resource = load("res://dialogue/lore.dialogue")
+
+func _ready():
+	var dialogue_line = await DialogueManager.get_next_dialogue_line(resource, "start") 
+	dialogue_line = await DialogueManager.get_next_dialogue_line(resource, dialogue_line.next_id)
+	
