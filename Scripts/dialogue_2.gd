cda8918ae857a2d0afe941575f6b22f6aff5e103
LaurenCatt <laurenknowles2772@outlook.com>
2025-09-28 02:32:31 -0400

    just working the designnnnn

--- Scripts/dialogue_2.gd
+++ Scripts/dialogue_2.gd
@@ -21,7 +21,7 @@ func _ready() -> void:
 		if dialogue_label.has_method("type_out"):
 			dialogue_label.type_out()
 			# Poll while typing so we can detect input to skip typing.
-			var prev_mouse_pressed: bool = Input.is_mouse_button_pressed(MouseButton.LEFT)
+			var prev_mouse_pressed: bool = Input.is_mouse_button_pressed(MouseButton.MOUSE_BUTTON_LEFT)
 			while dialogue_label.is_typing:
 				await get_tree().process_frame
 				# Check space/enter
@@ -30,7 +30,7 @@ func _ready() -> void:
 					input_pressed_during_typing = true
 					continue
 				# Check left mouse just pressed
-				var mouse_pressed: bool = Input.is_mouse_button_pressed(MouseButton.LEFT)
+				var mouse_pressed: bool = Input.is_mouse_button_pressed(MouseButton.MOUSE_BUTTON_LEFT)
 				if mouse_pressed and not prev_mouse_pressed:
 					dialogue_label.skip_typing()
 					input_pressed_during_typing = true
@@ -47,12 +47,12 @@ func _ready() -> void:
 			await get_tree().process_frame
 		else:
 			# Wait for spacebar or left-click
-			var prev_mouse: bool = Input.is_mouse_button_pressed(MouseButton.LEFT)
+			var prev_mouse: bool = Input.is_mouse_button_pressed(MouseButton.MOUSE_BUTTON_LEFT)
 			while true:
 				await get_tree().process_frame
 				if Input.is_action_just_pressed("ui_accept"):
 					break
-				var mouse_now: bool = Input.is_mouse_button_pressed(MouseButton.LEFT)
+				var mouse_now: bool = Input.is_mouse_button_pressed(MouseButton.MOUSE_BUTTON_LEFT)
 				if mouse_now and not prev_mouse:
 					break
 				prev_mouse = mouse_now
