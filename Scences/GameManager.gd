6fa133be9f9ea77b27e5a18923271613a9f131ee
rudlyy <rudy.charles1207@gmail.com>
2025-09-28 02:11:12 -0400

    fdghjk

--- Scences/GameManager.gd
+++ Scences/GameManager.gd
@@ -1,15 +1,9 @@
 extends Node2D
-@onready var rat1= $Rats
-@onready var rat2= $Rat2
-@onready var rat3= $Rat3
+
 
 func _process(delta: float) -> void:
-	if rat1.health<=0:
-		rat1.queue_free()
-	if rat2.health<=0:
-		rat2.queue_free()
-	if rat3.health<=0:
-		rat3.queue_free()
+	pass
+
 		
 		
 
